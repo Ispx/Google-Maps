2a4f3bb27ce8c9e3361775b3f54b140d1fbe3905
Isaque Paixao <isaquespx98@gmail.com>
2022-07-15 09:33:49 -0300

    add widgets zoom and location

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -68,16 +68,112 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                   ],
                 );
               }
-              return GoogleMap(
-                initialCameraPosition: value.initialCameraPosition,
-                onMapCreated: controller.changeMapsController,
-                markers: value.markers,
-                polylines: value.polylines,
-                onTap: controller.onTap,
-                myLocationButtonEnabled: true,
+              return Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  GoogleMap(
+                    initialCameraPosition: value.initialCameraPosition,
+                    onMapCreated: controller.changeMapsController,
+                    markers: Set<Marker>.from(value.markers),
+                    polylines: value.polylines,
+                    onTap: controller.onTap,
+                    myLocationButtonEnabled: false,
+                    zoomControlsEnabled: false,
+                  ),
+                  AddressesWidget(),
+                ],
               );
             },
           ),
+        ),
+      ),
+      floatingActionButton: SizedBox(
+        height: 200,
+        width: 60,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ZommButtomWidget(
+              onPressed: () => controller.zoomIn(),
+              icon: Icons.add,
+            ),
+            ZommButtomWidget(
+              onPressed: () => controller.zommOut(),
+              icon: Icons.remove,
+            ),
+            FloatingActionButton(
+              onPressed: () => controller.getCurrentLocation(),
+              child: Icon(
+                Icons.location_on,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddressesWidget extends StatefulWidget {
+  const AddressesWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AddressesWidget> createState() => _AddressesWidgetState();
+}
+
+class _AddressesWidgetState extends State<AddressesWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * .3,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.5),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: const [
+          SizedBox(height: 8),
+          Text('Endereços'),
+          SizedBox(height: 8),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Origem',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 8),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Destino',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 8),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Confirmar'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ZommButtomWidget extends StatelessWidget {
+  final VoidCallback onPressed;
+  final IconData icon;
+  const ZommButtomWidget(
+      {Key? key, required this.icon, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Material(
+        child: IconButton(
+          onPressed: () => onPressed(),
+          icon: Icon(icon),
         ),
       ),
     );
