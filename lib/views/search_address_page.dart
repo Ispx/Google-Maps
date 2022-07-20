c66c2ce438925372216ad3f45e5f76366277ce14
Isaque Paixao <isaquespx98@gmail.com>
2022-07-20 09:14:17 -0300

    cearted search address page

---
+++ lib/views/search_address_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_routes/controllers/maps_controller.dart';
+import 'package:google_maps_routes/views/home_page.dart';
+import 'package:provider/provider.dart';
+
+class SearchAddressPage extends StatefulWidget {
+  final String? hint;
+  const SearchAddressPage({Key? key, this.hint}) : super(key: key);
+
+  @override
+  State<SearchAddressPage> createState() => _SearchAddressPageState();
+}
+
+class _SearchAddressPageState extends State<SearchAddressPage> {
+  TextEditingController textEditingController = TextEditingController();
+  MapsController mapsController = MapsController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ChangeNotifierProvider.value(
+          value: mapsController,
+          builder: (context, _) {
+            return Consumer<MapsController>(
+              builder: (context, valueController, _) {
+                return Column(
+                  children: [
+                    TextField(
+                      controller: textEditingController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        labelText: 'Pesquisar endereço',
+                        hintText: widget.hint,
+                      ),
+                      onChanged: (e) async {
+                        await valueController.searchAddress(e);
+                      },
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            ...valueController.addressesPlaceMarks.map(
+                              (e) => InkWell(
+                                onTap: () => Navigator.pop(context, e),
+                                child: Column(
+                                  children: [
+                                    AddressWidget(address: e),
+                                    Divider(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
