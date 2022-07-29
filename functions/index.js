458b454a4a4873ed87d8bf018f2725621a82a8c1
Isaque Paixao <isaquespx98@gmail.com>
2022-07-29 09:42:07 -0300

    add

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+
+const db = admin.firestore();
+const batch = db.batch();
+
+exports.createNewDocumentInNotificationsCollection = functions.firestore
+    .document("routes/{documentId}")
+    .onCreate((snapshot, context) => {
+        var newDocRef = db.doc('notifications/${snapshot.id}');
+        batch.set(newDocRef, {
+            "data": "1"
+        })
+        return batch.commit();
+    });
+
+exports.createOnNotificationsCollectionAndLogger = functions.firestore
+    .document("notifications/{documentId}")
+    .onCreate((snapshot, context) => {
+        functions.logger.info("Notificação do documento " + snapshot.id + " enviado!", { structuredData: true });
+    });
+
+
+
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
+
+
+
