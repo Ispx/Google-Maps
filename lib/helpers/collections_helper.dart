b689fc4477e6c9089fa700eef528d6fc6bae8b61
Isaque Paixao <isaquespx98@gmail.com>
2022-08-04 09:16:19 -0300

    fix

--- lib/helpers/collections_helper.dart
+++ lib/helpers/collections_helper.dart
@@ -1,9 +1,9 @@
-enum CollectionsHelper { IN_ROUTER, ROUTERS }
+enum CollectionsHelper { EN_ROUTE, HISTORIC }
 
 extension CollectionsHelperExt on CollectionsHelper {
   static Map<CollectionsHelper, String> get _map => {
-        CollectionsHelper.IN_ROUTER: 'in-router',
-        CollectionsHelper.ROUTERS: 'routes',
+        CollectionsHelper.EN_ROUTE: 'en-route',
+        CollectionsHelper.HISTORIC: 'historic',
       };
 
   String get getString => _map[this]!;
