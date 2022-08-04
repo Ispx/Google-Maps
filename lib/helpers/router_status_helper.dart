11f168f89af2826449f805d3e63bf0cbc8edc79d
Isaque Paixao <isaquespx98@gmail.com>
2022-08-04 09:16:13 -0300

    add status CREATED

--- lib/helpers/router_status_helper.dart
+++ lib/helpers/router_status_helper.dart
@@ -1,7 +1,8 @@
-enum RouterStatusHelper { ONGOING, DONE, CANCELED, CANCEL, END }
+enum RouterStatusHelper { CREATED, ONGOING, DONE, CANCELED, CANCEL, END }
 
 extension RouterStatusHelperExt on RouterStatusHelper {
   static Map<RouterStatusHelper, String> get mapToString => {
+        RouterStatusHelper.CREATED: "Criado",
         RouterStatusHelper.ONGOING: "Em andamento",
         RouterStatusHelper.CANCELED: "Cancelado",
         RouterStatusHelper.DONE: "Finalizado",
